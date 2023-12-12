67104d5028e5a6636743a30bd3103102b0a63afc
Erik Wiederstrøm <wiederstromerik@gmail.com>
2023-12-12 15:06:57 +0100

    Second commit: Updated doctrings in code

--- input_list.py
+++ input_list.py
@@ -1,9 +1,14 @@
 class AddingToList():
+    '''List generator for integers'''
+    '''Method for validating input'''
     def __init__(self):
+        '''List of validated integers'''
         self._accepted_input = []
+        '''Invalid input'''
         self._discard_input = []
 
     def validator(self, user_input):
+        '''Checks if elements in list is a positive integer, discards invalid input'''
         for element in user_input:
             try:
                 converted_element = int(element)
@@ -14,6 +19,7 @@ class AddingToList():
             except ValueError:
                 self._discard_input.append(element)
 
+        '''Checks if list of accepeted integers is of correct length, compared to discard input and total lengt of input  '''
         if (len(self._accepted_input) + len(self._accepted_input) == len(user_input)) and (i > 0 for i in self._accepted_input):
             return self._accepted_input
         else:
